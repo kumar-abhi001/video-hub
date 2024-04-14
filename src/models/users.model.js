8308568cc9b822459c76d6e097fed0166d3bd71b
kumar-abhi001 <kumar.abhi.ok@gmail.com>
2024-04-15 01:17:06 +0530

    token creation

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const usersSchema = new Schema(
   {
@@ -47,5 +49,54 @@ const usersSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// adding middleware to update our hashed password when password is updated
+usersSchema.pre("save", async function (next) {
+  if (this.isModified("password") === true) {
+    try {
+      const saltRound = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, saltRound);
+    } catch (error) {
+      console.log("ERROR in password hashing: ", error);
+    }
+  }
+  next();
+});
+
+//method to check password is correct or not
+usersSchema.methods.checkPassword = async function (password) {
+  return bcrypt.compare(password, this.password); // return boolean value
+}
+
+//generate access token
+usersSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+//adding method to creating refresh token
+usersSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      password: this.password
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+
 
 export const Users = mongoose.model("Users", usersSchema);
